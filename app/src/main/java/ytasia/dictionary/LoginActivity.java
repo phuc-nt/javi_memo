efbb77ca73b12b80b36982258a5c181f7f68694a
trinhnd <trinhnd@yz-japan.tokyo>
2016-03-17 12:09:43 +0900

    Login Facebook insert

--- app/src/main/java/ytasia/dictionary/LoginActivity.java
+++ app/src/main/java/ytasia/dictionary/LoginActivity.java
@@ -5,39 +5,70 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+
+
 
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText usernameTx;
-    private EditText passwordTx;
     private Button signinBt;
-    private Button signupBt;
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
         setContentView(R.layout.activity_login);
-
-        usernameTx = (EditText) findViewById(R.id.login_username_input);
-        passwordTx = (EditText) findViewById(R.id.login_password_index);
         signinBt = (Button) findViewById(R.id.login_sign_in_button);
-        signupBt = (Button) findViewById(R.id.login_sign_up_button);
-
-        signinBt.setOnClickListener(new View.OnClickListener() {
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
-            public void onClick(View v) {
+            public void onSuccess(final LoginResult loginResult) {
+                AccessToken tok;
+                tok = AccessToken.getCurrentAccessToken();
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                intent.putExtra("Userid", tok.getUserId());
                 startActivity(intent);
             }
-        });
 
-        signupBt.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
-                startActivity(intent);
+            public void onCancel() {
+                Toast.makeText(LoginActivity.this, "Login attempt canceled.", Toast.LENGTH_LONG).show();
             }
+
+            @Override
+            public void onError(FacebookException error) {
+                error.printStackTrace();
+                Toast.makeText(LoginActivity.this, "Login attempt failed.", Toast.LENGTH_LONG).show();
+            }
+
         });
+       signinBt.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+               intent.putExtra("Userid","Guest");
+               startActivity(intent);
+           }
+       });
+
+
     }
+    @Override
+   protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+
 }
